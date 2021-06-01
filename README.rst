ee4b7e27ce2791158cc9359f9fd2edc64979f8f3
Nizar Alsaeed <nizar.ayman@gmail.com>
2021-06-01 23:26:14 +0300

    lab 18 done

---
+++ README.rst
@@ -0,0 +1,11 @@
+Lab 18: Cryptography
+/////////////////////
+
+we’ll be tackling a cryptographic classic - the Caesar Cipher.
+
+Author: Nizar Alsaeed
+
+version: 1.0.0
+
+
+`PR <https://github.com/NizarAlsaeed/caesar-cipher/pull/1>`_
--- tests/test_caesar_cipher.py
+++ tests/test_caesar_cipher.py
@@ -1,5 +1,29 @@
 from caesar_cipher import __version__
-
+from caesar_cipher.cipher import encrypt, decrypt, crack
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_encrypt():
+    """encrypt a string with a given shift
+    encryption should handle upper and lower case letters
+    encryption should allow non-alpha characters but ignore them, including white space
+    """
+    actual = encrypt('i Did not find it, please go back!',5)
+    expected = 'n ini sty knsi ny uqjfxj lt gfhp'
+    assert actual == expected
+
+def test_decrypt():
+    """decrypt a previously encrypted string with the same shift
+    """
+    actual = decrypt('n ini sty knsi ny uqjfxj lt gfhp',5)
+    expected = 'i did not find it please go back'
+    assert actual == expected    
+
+def test_crack():
+    """decrypt encrypted version of It was the best of times, it was the worst of times. WITHOUT knowing the shift used."""
+    actual = crack('n ini sty knsi ny uqjfxj lt gfhp')
+    expected = 'i did not find it please go back'
+    assert actual == expected      
+
+
